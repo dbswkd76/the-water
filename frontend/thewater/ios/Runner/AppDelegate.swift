7996d6270be6da398c883c0d5b95123699ceb86c
dbswkd76 <dbswkd76@naver.com>
2025-06-02 10:28:39 +0900

    Remove Google API Key from source code

--- frontend/thewater/ios/Runner/AppDelegate.swift
+++ frontend/thewater/ios/Runner/AppDelegate.swift
@@ -9,7 +9,7 @@ import GoogleMaps
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
-    GMSServices.provideAPIKey("***REMOVED***")
+    GMSServices.provideAPIKey("")
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
